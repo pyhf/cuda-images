3bec9beb4a8028c442806a30c5fb4e4ea080ae30
Matthew Feickert <matthew.feickert@cern.ch>
2021-06-04 02:58:10 -0500

    Add backend testing to test script

--- tests/test_backend.sh
+++ tests/test_backend.sh
@@ -1,13 +1,33 @@
 #!/bin/bash
 
-if [[ ! -d nvidia-gpu-ml-library-test ]];then
-    git clone --depth 1 https://github.com/matthewfeickert/nvidia-gpu-ml-library-test.git
-fi
-cd nvidia-gpu-ml-library-test || exit
 
-printf "\n# nvidia-smi --list-gpus\n"
-nvidia-smi --list-gpus
-printf "\n# python jax_detect_GPU.py\n"
-python jax_detect_GPU.py
-printf "\n# python jax_MNIST.py\n"
-python jax_MNIST.py
+function main() {
+    # 1: pyhf backend name
+
+    pyhf_backend_name=""
+    if [[ $# -gt 0 ]];then
+        pyhf_backend_name=$(echo "${1}" | awk '{print tolower($0)}')
+    fi
+
+    if [[ ! -d nvidia-gpu-ml-library-test ]];then
+        git clone --depth 1 https://github.com/matthewfeickert/nvidia-gpu-ml-library-test.git
+    fi
+    cd nvidia-gpu-ml-library-test || exit
+
+    printf "\n# nvidia-smi --list-gpus\n"
+    nvidia-smi --list-gpus
+
+    if [[ "${pyhf_backend_name}" = "jax" ]]; then
+        printf "\n# python jax_detect_GPU.py\n"
+        python jax_detect_GPU.py
+        printf "\n# python jax_MNIST.py\n"
+        python jax_MNIST.py
+    elif [[ "${pyhf_backend_name}" =~ ^("pytorch"|"torch")$ ]]; then
+        printf "\n# python torch_detect_GPU.py\n"
+        python torch_detect_GPU.py
+        printf "\n# python torch_MNIST.py\n"
+        python torch_MNIST.py
+    fi
+}
+
+main "$@" || exit 1
