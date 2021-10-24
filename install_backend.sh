5dfab8f6664ad0bdece06869d974c05c3259b343
Matthew Feickert <matthew.feickert@cern.ch>
2021-10-24 17:50:29 -0500

    fix: Add CuDNN version information to jaxlib install (#11)
    * Determine available version of CuDNN and install that CUDA enabled version of jaxlib
       - Use /usr/include/x86_64-linux-gnu/cudnn_version_v*.h to get CuDNN version numbers

--- install_backend.sh
+++ install_backend.sh
@@ -9,17 +9,22 @@ function install_jax_backend {
     python -m pip --no-cache-dir install --upgrade jax jaxlib
     jaxlib_version=$(python -c 'import jaxlib; print(jaxlib.__version__)')
     # shellcheck disable=SC2153
-    cuda_version=$(echo "${CUDA_VERSION}" | cut -d . -f -2 | sed 's/\.//')
+    cuda_version=$(echo "${CUDA_VERSION}" | cut --delimiter . --fields -1)
+    # Determine CuDNN version number
+    CUDNN_MAJOR="$(cat /usr/include/x86_64-linux-gnu/cudnn_version_v*.h | grep '#define CUDNN_MAJOR' | awk '{print $NF}')"
+    CUDNN_MINOR="$(cat /usr/include/x86_64-linux-gnu/cudnn_version_v*.h | grep '#define CUDNN_MINOR' | awk '{print $NF}')"
+    CUDNN_PATCHLEVEL="$(cat /usr/include/x86_64-linux-gnu/cudnn_version_v*.h | grep '#define CUDNN_PATCHLEVEL' | awk '{print $NF}')"
+    cudnn_version="${CUDNN_MAJOR}${CUDNN_MINOR}${CUDNN_PATCHLEVEL}"
     echo "jaxlib version: ${jaxlib_version}"
-    echo "jaxlib+cuda version: ${jaxlib_version}+cuda${cuda_version}"
-    python -m pip --no-cache-dir install --upgrade jax jaxlib=="${jaxlib_version}+cuda${cuda_version}" --find-links https://storage.googleapis.com/jax-releases/jax_releases.html
+    echo "jaxlib+cuda version: ${jaxlib_version}+cuda${cuda_version}.cudnn${cudnn_version}"
+    python -m pip --no-cache-dir install --upgrade jax jaxlib=="${jaxlib_version}+cuda${cuda_version}.cudnn${cudnn_version}" --find-links https://storage.googleapis.com/jax-releases/jax_releases.html
 }
 
 function install_pytorch_backend {
     local torch_version=1.9.0
     local cuda_version
 
-    cuda_version=$(echo "${CUDA_VERSION}" | cut -d . -f -2 | sed 's/\.//')
+    cuda_version=$(echo "${CUDA_VERSION}" | cut --delimiter . --fields -2 | sed 's/\.//')
     echo "torch+cuda version: ${torch_version}+cu${cuda_version}"
     python -m pip --no-cache-dir install torch=="${torch_version}+cu${cuda_version}" --find-links https://download.pytorch.org/whl/torch_stable.html
 }
